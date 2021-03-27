2217d99c47b281a94cf3f2ad1cdb5fa508426ef7
Prosper <prompt2k2@gmail.com>
2021-03-27 01:56:19 +0100

    Signed-off-by: Prosper <prompt2k2@gmail.com>

--- Todo/Todos/urls.py
+++ Todo/Todos/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='todos-home'),
+    path('add_todo/', views.add_todo),
 ]
--- Todo/Todos/views.py
+++ Todo/Todos/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from .models import Tasks
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from Todos.models import Tasks
+from django.http import HttpResponseRedirect
 # Create your views here.
+
 def index(request):
-    return render(request, 'main/index.html')
+    todo_items = Tasks.objects.all().order_by ("-added_date")
+    return render(request, 'main/index.html', {"todo_items": todo_items})
+
+@csrf_exempt
+def add_todo(request):
+    current_date = timezone.now()
+    content = request.POST['content']
+    NewItem = Tasks.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect("/")
